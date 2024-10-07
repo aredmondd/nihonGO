66753b74c5db8824c394ca6a168c07db6a2395ee
masterjedidcfl <masterjedidcfl@gmail.com>
2024-10-07 12:12:54 -0400

    User registration/login/logout functionality

--- nihonGO/myapp/urls.py
+++ nihonGO/myapp/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="home")
+    path('', views.home, name="home"),
+
+    path('register', views.register, name="register"),
+
+    path('my-login', views.my_login, name="my-login"),
+
+    path('profile-page', views.profile_page, name="profile-page"),
+
+    path('user-logout', views.user_logout, name="user-logout"),
+
+    
 ]
--- nihonGO/myapp/views.py
+++ nihonGO/myapp/views.py
@@ -1,6 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from . forms import CreateUserForm, LoginForm
+
+# - Authentication Models and Functions
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def home (request):
-    return HttpResponse('Hello World!')
+    return render(request, 'myapp/base.html')
+
+def profile_page (request):
+    return render(request, 'myapp/profile-page')
+
+
+def register(request):
+
+    form = CreateUserForm()
+
+    if request.method == "POST":
+
+        form = CreateUserForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("my-login")
+        
+    
+    context = {'registerform':form}
+
+
+    return render(request, 'myapp/register.html', context=context)
+
+
+def my_login(request):
+
+    form = LoginForm()
+
+    if request.method == 'POST':
+
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+
+                auth.login(request, user)
+
+                return redirect("home")
+            
+    
+    context = {'loginform':form}
+
+    return render(request, 'myapp/login.html', context=context)
+
+
+def user_logout(request):
+
+    auth.logout(request)
+
+    return redirect("home")
