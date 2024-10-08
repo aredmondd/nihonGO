8a8fc23262b3f14dd610eb06ae0a2884c9d629bf
“Aiden <adnrdmnd@gmail.com>
2024-10-08 12:45:31 -0400

    alex photo on AA

--- nihonGO/nihonGO/urls.py
+++ nihonGO/nihonGO/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('myapp.urls')),
     path('nihongo/', include('theme.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
     path('flashcards/', include('flashcards.urls')),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
