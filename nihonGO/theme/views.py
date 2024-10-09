8f284be813344fdf7facaec013b0a85ed5130d3e
“Aiden <adnrdmnd@gmail.com>
2024-10-09 10:11:18 -0400

    pretty heavy refactors for readability

--- nihonGO/theme/views.py
+++ nihonGO/theme/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return render(request, 'layout.html')
+    return render(request, 'index.html')
 
 def login (request):
     return render(request, 'login.html')
