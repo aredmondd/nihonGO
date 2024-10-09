181d2182ec4196e392f849a853e656e01603cb00
“Aiden <adnrdmnd@gmail.com>
2024-10-08 21:52:43 -0400

    fucking around with sidenavbar

--- nihonGO/theme/views.py
+++ nihonGO/theme/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index (request):
-    return render(request, 'index.html')
+    return render(request, 'layout.html')
 
 def login (request):
     return render(request, 'login.html')
