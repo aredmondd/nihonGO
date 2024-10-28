2c9cca34e38d7a2c70ad74b4290625d37ead83f0
masterjedidcfl <masterjedidcfl@gmail.com>
2024-10-27 21:01:37 -0400

    pfp now appears and can be changed within admin panel

--- nihonGO/myapp/models.py
+++ nihonGO/myapp/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    avatar = models.ImageField(default='nihonGO\nihonGO\theme\media\default.jpg', upload_to='profile_images')
     bio = models.TextField()
 
     def __str__(self):
--- nihonGO/nihonGO/settings.py
+++ nihonGO/nihonGO/settings.py
@@ -139,7 +139,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Media Root for User Uploaded Files/ Working With Images
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'theme/media')
 MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend" # new
