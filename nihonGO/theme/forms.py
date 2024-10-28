6ade52da310ede6b361842f7809e62ce420f1fc2
“Aiden <adnrdmnd@gmail.com>
2024-10-28 12:31:56 -0400

    uhhh

--- nihonGO/theme/forms.py
+++ nihonGO/theme/forms.py
@@ -18,28 +18,28 @@ class FlashcardForm(forms.ModelForm):
 class CreateUserForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(attrs={
-            'class': 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'class': 'border border-black border-2 text-black rounded-lg text-sm',
             'placeholder': 'Username',
             'required': True
         })
     )
     email = forms.EmailField(
         widget=forms.EmailInput(attrs={
-            'class': 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'class': 'border border-black border-2 text-black rounded-lg text-sm',
             'placeholder': 'Email',
             'required': True
         })
     )
     password1 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'class': 'border border-black border-2 text-black rounded-lg text-sm',
             'placeholder': 'Password',
             'required': True
         })
     )
     password2 = forms.CharField(
         widget=forms.PasswordInput(attrs={
-            'class': 'bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500',
+            'class': 'border border-black border-2 text-black rounded-lg text-sm',
             'placeholder': 'Confirm Password',
             'required': True
         })
