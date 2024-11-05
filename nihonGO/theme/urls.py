0b783fa1b9db9c0693a113041d15fb4ae8e8023e
Alex Richardson <laurenrichardson@Alexs-MacBook-Pro-6.local>
2024-11-04 19:52:18 -0500

    URL fix

--- nihonGO/theme/urls.py
+++ nihonGO/theme/urls.py
@@ -1,26 +1,32 @@
 from django.urls import path, include
-from django.contrib import admin
 from . import views
+from .views import profile
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('login/', views.login, name="login"),
+    path('login/', views.my_login, name="login"),
     path('register/', views.register, name="register"),
     path('about/', views.about, name="about"),
     path('dashboard/', views.dashboard, name="dashboard"),
     path('forum/', views.forum, name="forum"),
     path('my-profile/', views.profile, name="my-profile"),
-    path('profile-page/', views.profile_page, name="profile-page"),
-    path('user-logout/', views.user_logout, name="user-logout"),
-    path('profile/', views.profile, name='users-profile'),
+    path('messages/', views.messages, name="messages"),
+    path('mydecks/', views.my_decks, name="my_decks"),
+    path('add/', views.add_deck, name='add_deck'),
+    path('study/<int:deck_id>/', views.study, name='study'),
+    path('profile-page', views.profile_page, name="profile-page"),
+    path('user-logout', views.user_logout, name="user-logout"),
+    path('profile/', profile, name='users-profile'),
     path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-    #this is not correct
-    path('chat/', include('ChitChat.urls')),
+    path('edit/<int:deck_id>/', views.edit_deck, name='edit_deck'),
+    path('delete/<int:deck_id>/', views.delete_deck, name='delete_deck'),  
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
 
 # URL patterns specific to ChitChat app
