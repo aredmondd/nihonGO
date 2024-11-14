e5d1fdb2bf1eb9558f436651de4a6fef06ba25c9
masterjedidcfl <masterjedidcfl@gmail.com>
2024-11-14 15:31:06 -0500

    PFP, username and email showing up in user profile

--- nihonGO/theme/urls.py
+++ nihonGO/theme/urls.py
@@ -4,6 +4,8 @@ from .views import profile, ChangePasswordView
 from django.contrib.auth import views as auth_views
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
@@ -36,4 +38,5 @@ urlpatterns = [
 
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
